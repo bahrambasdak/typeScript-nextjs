530d593bc385a8188c2b789c2325f7d4a8b15bee
bahram <bahrambasdak@gmail.com>
2024-05-09 23:43:08 +0330

    fix tabs

--- app/(pages)/(home)/layout.tsx
+++ app/(pages)/(home)/layout.tsx
@@ -1,13 +1,16 @@
+"use client"
 import Navbar from "@/app/components/navbar";
+import { useState } from "react";
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [activeTab,setActiveTab] = useState('');
   return (
     <>
-      <Navbar />
+      <Navbar activeTab={activeTab} setActiveTab={setActiveTab}/>
       {children}
     </>
   );
--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,13 +1,13 @@
-const Navbar = () => {
-    return ( <div className="flex  h-28 justify-evenly items-center *:group-[btn]:bg-red gap-4">
-        <button className="p-4 bg-gray-50 rounded-md grow">11111</button>
-        <button className="p-4 bg-gray-50 rounded-md grow">2222</button>
-        <button className="p-4 bg-gray-50 rounded-md grow">33333</button>
-        <button className="p-4 bg-gray-50 rounded-md grow">4444</button>
-        <button className="p-4 bg-gray-50 rounded-md grow">55555</button>
+const Navbar = ({activeTab='tab1' , setActiveTab=(tab:string)=>{}}) => {
+  return (
+    <div className="flex w-full px-10  h-28 justify-around items-center  gap-4 transition-all">
+      <button onClick={()=>setActiveTab('tab1')} className="p-4 bg-gray-50 rounded-md grow transition-all" style={{backgroundColor:activeTab==='tab1'?'blue':'gray'}}>dsfsdfsdf</button>
+      <button  onClick={()=>setActiveTab('tab2')} className="p-4 bg-gray-50 rounded-md grow transition-all" style={{backgroundColor:activeTab==='tab2'?'blue':'gray'}}>2222</button>
+      <button  onClick={()=>setActiveTab('tab3')} className="p-4 bg-gray-50 rounded-md grow transition-all"style={{backgroundColor:activeTab==='tab3'?'blue':'gray'}}>33333</button>
+      <button  onClick={()=>setActiveTab('tab4')} className="p-4 bg-gray-50 rounded-md grow transition-all"style={{backgroundColor:activeTab==='tab4'?'blue':'gray'}}>4444</button>
+      <button  onClick={()=>setActiveTab('tab5')} className="p-4 bg-gray-50 rounded-md grow transition-all"style={{backgroundColor:activeTab==='tab5'?'blue':'gray'}}>55555</button>
+    </div>
+  );
+};
 
-        
-    </div> );
-}
- 
 export default Navbar;
